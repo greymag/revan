bed2e93a12bf429947dbab139eecec9e8329de67
greymag <greymag@gmail.com>
2024-08-22 18:14:08 +0200

    Sample projects

---
+++ src/main/kotlin/example/com/Application.kt
@@ -0,0 +1,17 @@
+package example.com
+
+import example.com.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureAdministration()
+    configureSerialization()
+    configureMonitoring()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
+}
