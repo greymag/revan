e45757a2af93d46e5eb0615eea5209d609b84c05
greymag <greymag@gmail.com>
2025-03-10 18:10:42 +0100

    Disable kotlinx-serialization endpoint

--- src/main/kotlin/net/orangedog/revan/plugins/Serialization.kt
+++ src/main/kotlin/net/orangedog/revan/plugins/Serialization.kt
@@ -3,16 +3,14 @@ package net.orangedog.revan.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json()
     }
-    routing {
-        get("/json/kotlinx-serialization") {
-                call.respond(mapOf("hello" to "world"))
-            }
-    }
+//    routing {
+//        get("/json/kotlinx-serialization") {
+//                call.respond(mapOf("hello" to "world"))
+//            }
+//    }
 }
