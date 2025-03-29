fc33b8da3215a772ec1e0d5b945d141371d9ad5f
greymag <greymag@gmail.com>
2025-03-29 18:06:21 +0100

    Temporary disable shutdown endpoint

--- src/main/kotlin/net/orangedog/revan/plugins/Administration.kt
+++ src/main/kotlin/net/orangedog/revan/plugins/Administration.kt
@@ -1,14 +1,13 @@
 package net.orangedog.revan.plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.response.*
 
 fun Application.configureAdministration() {
-    install(ShutDownUrl.ApplicationCallPlugin) {
-        // The URL that will be intercepted (you can also use the application.conf's ktor.deployment.shutdown.url key)
-        shutDownUrl = "/ktor/application/shutdown"
-        // A function that will be executed to get the exit code of the process
-        exitCodeSupplier = { 0 } // ApplicationCall.() -> Int
-    }
+    // TODO: add some security to this and uncomment
+//    install(ShutDownUrl.ApplicationCallPlugin) {
+//        // The URL that will be intercepted (you can also use the application.conf's ktor.deployment.shutdown.url key)
+//        shutDownUrl = "/ktor/application/shutdown"
+//        // A function that will be executed to get the exit code of the process
+//        exitCodeSupplier = { 0 } // ApplicationCall.() -> Int
+//    }
 }
