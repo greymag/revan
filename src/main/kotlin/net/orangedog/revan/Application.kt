84d4c06e279c4b9cb43711c6e7eac930f8bb6cac
greymag <greymag@gmail.com>
2025-03-29 19:37:09 +0100

    Check for empty MongoDB connection String

--- src/main/kotlin/net/orangedog/revan/Application.kt
+++ src/main/kotlin/net/orangedog/revan/Application.kt
@@ -5,19 +5,24 @@ import net.orangedog.revan.modules.admin.AdminModuleConfig
 import net.orangedog.revan.modules.trubar.TrubarModuleConfig
 import net.orangedog.revan.plugins.*
 
+private const val CONFIG_MONGODB_CONNECTION_STRING = "mongodb.connectionString"
+
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
-    val mongoDBConnectionString = environment.config.propertyOrNull("mongodb.connectionString")?.getString()
-    // TODO: check if the connection string is not null and not empty
+    val mongoDBConnectionString = environment.config.propertyOrNull(CONFIG_MONGODB_CONNECTION_STRING)?.getString()
+    if (mongoDBConnectionString.isNullOrEmpty()) {
+        throw IllegalArgumentException("MongoDB connection string is not set. " +
+                "Set parameter $CONFIG_MONGODB_CONNECTION_STRING in the configuration file.")
+    }
 
     val adminConfig = AdminModuleConfig.from(environment.config)
     val trubarConfig = TrubarModuleConfig.from(environment.config)
 
     configureAdministration()
-    val db = configureDatabases(mongoDBConnectionString!!)
+    val db = configureDatabases(mongoDBConnectionString)
     configureDependencyInjection(db, trubarConfig)
     configureAuthentication(adminConfig)
     configureSerialization()
